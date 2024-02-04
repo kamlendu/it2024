92d3962f962da717ff2d3653c1ba5fc2f172ae9b
kamlendu <kamlendup@gmail.com>
2024-02-04 21:39:25 +0530

    srv2

---
+++ EJBMessagingModule/src/java/ejb/FirstQueueBean.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/MessageDrivenBean.java to edit this template
+ */
+package ejb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author root
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/itqueue"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class FirstQueueBean implements MessageListener {
+    
+    public FirstQueueBean() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        String msg=null;
+        try{
+            
+          if(message instanceof TextMessage)
+          {
+               msg = ((TextMessage)message).getText();
+         
+             System.out.println("First Queue Bean has got the message -- " + msg);
+          }
+        
+        }
+        catch(Exception e)
+        {
+           e.printStackTrace();
+        }
+        
+        
+    }
+    
+}
---
+++ EJBMessagingModule/src/java/ejb/FirstTopicBean.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/MessageDrivenBean.java to edit this template
+ */
+package ejb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author root
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "client1"),
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/ittopic"),
+    @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "jms/ittopic"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+})
+public class FirstTopicBean implements MessageListener {
+    
+    public FirstTopicBean() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        
+        
+        String msg=null;
+        try{
+            
+          if(message instanceof TextMessage)
+          {
+               msg = ((TextMessage)message).getText();
+         
+             System.out.println("First Topic Bean has got the message -- " + msg);
+          }
+        
+        }
+        catch(Exception e)
+        {
+           e.printStackTrace();
+        }
+    }
+    
+}
