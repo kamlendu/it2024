29f0104ec634654f15b89150eb426dddb445ec75
kamlendu <kamlendup@gmail.com>
2024-02-27 23:58:48 +0530

    srv3

---
+++ JSFApp/src/main/java/converter/MyConverter.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author root
+ */
+@FacesConverter("myconverter")
+public class MyConverter implements Converter {
+
+    //Model Value
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+        return "ABC-"+ value;
+    }
+
+    // Component Value
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+        String val = ((String)value).substring(4);
+        
+     return val;
+    }
+    
+    
+    
+}
